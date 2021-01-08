d6bfcbdf902c2b794fb712791b8e4271f359a0df
Masnc7 <masnc7@gmail.com>
2021-01-08 20:38:44 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button,Navbar,NavDropdown,Nav,Form,FormControl } from 'react-bootstrap'
+
+export const NavBar = () => {
+    return(
+    <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">Sierra</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Inicio</Nav.Link>
+        <Nav.Link href="#link">Carrito</Nav.Link>
+        <NavDropdown title="Categorias" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Productos</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Servicios</NavDropdown.Item>
+          </NavDropdown>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-success">Buscar</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
+    )
+} 
